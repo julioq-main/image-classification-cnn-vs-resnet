154c01a4451360319b99372875a735e84d946b0d
julioq-main <julioq.main@gmail.com>
2026-04-07 18:41:54 +0200

    [ADD] Parser for terminal command

--- main.py
+++ main.py
@@ -3,12 +3,23 @@
 """
 
 import yaml
+import argparse
 from training.train import run_training
 from utils.seed import set_seed
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog="Parser training and benchmarking models",)
+    parser.add_argument("--config", "-c", type=str, default="experiments/testing/config.yaml", required=False, help="Path to the YAML config file (e.g. config.yaml)")
+    #Add more arguments and implement logic
+    parser.add_argument("--save-dir", "-sd", type=str, required=False, help="Path to the directory to save")
+    parser.add_argument("--training", "-t", type=bool, required=False, default=True)
+    return parser.parse_args()
+
 def main():
-    
-    with open("experiments/test/config.yaml", "r") as f:
+    args = parse_args()
+
+    with open(args.config, "r") as f:
         config = yaml.safe_load(f)
 
     seed = config.get("seed", None)
